60360da9b7461e964ccdfdf2c5c86d270d87f417
Weelapat <38096723+BillyBacker@users.noreply.github.com>
2024-04-28 01:02:28 +0700

    Add root

---
+++ root.go
@@ -0,0 +1,8 @@
+package root
+
+import (
+	_ "github.com/Asungha/Grizzly/controller"
+	_ "github.com/Asungha/Grizzly/eventbus"
+	_ "github.com/Asungha/Grizzly/exception"
+	_ "github.com/Asungha/Grizzly/utils"
+)
